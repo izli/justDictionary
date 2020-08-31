da4718a822b3c84cf15b57de967341dbcb923e9a
Isa Virtaoja <isa.virtaoja@gmail.com>
2020-08-31 11:05:13 +0300

    Print thesaurus data
    Reformat and rename for clarity

--- src/PrintThesaurus.js
+++ src/PrintThesaurus.js
@@ -1,31 +1,35 @@
 import React from 'react';
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
 
 const createStyles = makeStyles(() => ({
-  description: {
-    display: 'flex',
-    color: 'green',
-    flexDirection: 'row',
-    paddingLeft: '8px',
-  },
-  descContainer: {
+  typeContainer: {
     display: 'flex',
     flexDirection: 'column',
+    paddingLeft: '8px',
   },
   allDesc: { color: 'blue' },
 
-  synItem: {
+  descContainer: {},
+  descText: {
     display: 'flex',
-    paddingLeft: '8px',
-    paddingRight: '8px',
+    color: 'green',
+    flexDirection: 'row',
   },
+
   synList: {
     flexDirection: 'row',
     display: 'flex',
     flexWrap: 'wrap',
     marginBottom: '16px',
+  },
+  synItem: {
+    display: 'flex',
+    paddingRight: '8px',
+  },
+  subHeader: {
+    color: 'orange',
   },
 }));
 
@@ -43,23 +47,30 @@ export function PrintThesaurus(props) {
           </Typography>
         );
       }
-      console.log('all syns', { allSyns });
       allDesc.push(
-        <div>
+        <div className={myStyles.descContainer}>
           <Typography
             component={'span'}
             variant="body1"
-            className={myStyles.description}
+            className={myStyles.descText}
           >
             Description {i}: {props.data[0].values[i].desc}
           </Typography>
-          <div className={myStyles.synList}>{allSyns}</div>
+          <Typography>Example: {props.data[0].values[i].example}</Typography>
+          <div>
+            <Typography variant="body1" className={myStyles.subHeader}>
+              Synonyms
+            </Typography>
+            <div className={myStyles.synList}>{allSyns}</div>
+          </div>
         </div>
       );
     }
 
+    //LOOP FOR VERBS
+
     return (
-      <div className={myStyles.descContainer}>
+      <div className={myStyles.typeContainer}>
         <Typography variant="h5">Type: {props.data[0].type}</Typography>
         <div className={myStyles.allDesc}>{allDesc}</div>
       </div>
