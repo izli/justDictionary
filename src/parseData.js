97d1324f558a2d66e50d63cac7c665687a543757
Isa Virtaoja <isa.virtaoja@gmail.com>
2020-09-02 13:38:33 +0300

    Parse dictionary data into more usable format.
    Include from source data only those details, where meta.id is either "searchWord" or in a format "searchWord:[number]". This will include only primary data. Use function dictParseItem and regex for this. Instead of one big mapping function, use multiple functions for mapping the data (from parseDictData -> buildDictionaryResponse -> dictParseItem -> collect definitions with dictParseDefinitionText and explanations with dictParseExplanantionText)
    
    Also rename functions for clarity to include thesaurus/dictionary

--- src/parseData.js
+++ src/parseData.js
@@ -1,4 +1,4 @@
-export function parseData(allDataArray) {
+export function parseThesData(allDataArray) {
   let array2 = allDataArray.map((element1) => ({
     type: element1.fl,
     values: element1.def[0].sseq.map((element2) => ({
@@ -10,7 +10,61 @@ export function parseData(allDataArray) {
   }));
 
   return array2;
-  /*
+}
+
+export function parseDictData(allDictDataArray, searchWord) {
+  // debugger;
+  let tempArray = buildDictionaryResponse(allDictDataArray, searchWord);
+  // let parsedData = allDictDataArray.map((element1) => ({
+  //   type: element1.fl,
+  //   def1Type: element1.def[0].vd,
+  //   def1Values: element1.def[0].sseq.map((element2) => ({
+  //     desc: element2[0][1].dt[0][1],
+  //     example: element2[0][1].dt[1][1], //[1][0].t,
+  //   })),
+  // }));
+  // console.log('result', parsedData);
+  // return parsedData;
+  return tempArray;
+}
+
+export function buildDictionaryResponse(data, searchWord) {
+  return data.map((item) => dictParseItem(item, searchWord));
+}
+
+function dictParseItem(item, searchWord) {
+  let regPattern = '^' + searchWord + ':?\\b';
+  let reg = new RegExp(regPattern, 'g');
+  let tempMatch = item.meta.id.search(reg);
+  if (tempMatch > -1) {
+    return {
+      definitions: item.def[0].sseq.map((definition) =>
+        dictParseDefinitionText(definition)
+      ),
+      explanations: item.def[0].sseq.map((definition) =>
+        dictParseExplanationText(definition)
+      ),
+    };
+  } else return;
+}
+
+function dictParseDefinitionText(definition) {
+  // debugger;
+  let tempDef = definition[0][1].dt[0][1];
+  return tempDef;
+}
+
+function dictParseExplanationText(definition) {
+  // debugger;
+  if (definition[0][1].dt.length > 1) {
+    let tempExp = definition[0][1].dt[1][1][0].t;
+    let tempT = tempExp.t;
+    // debugger;
+    return tempExp;
+  }
+  return;
+}
+/*
 SYNS
   data[0].values[0].syns[0].wd
   "arrangement"
@@ -30,15 +84,14 @@ RELS
     onSetParsedArray(array2);
   }
   */
-  //   return (
-  //     <div>
-  //       <div>you searched for: {props.searchWord}</div>
-  //       <div>all data array length: {array2.length}</div>
-  //       <ul>
-  //         {array2.map((item) => {
-  //           return <li key="abc">{item.length}</li>;
-  //         })}
-  //       </ul>
-  //     </div>
-  //   );
-}
+//   return (
+//     <div>
+//       <div>you searched for: {props.searchWord}</div>
+//       <div>all data array length: {array2.length}</div>
+//       <ul>
+//         {array2.map((item) => {
+//           return <li key="abc">{item.length}</li>;
+//         })}
+//       </ul>
+//     </div>
+//   );
