c3c97c9c0a4f6665bb178491b274f271b480fb9c
Isa Virtaoja <isa.virtaoja@gmail.com>
2020-09-02 13:34:38 +0300

    Rename functions and files for clarity to include thesaurus/dictionary in the name
    Get dictionary data with fetch with useGetDictionary(searchWord, setDictResults)
    Create logic for handling dictionary data

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { parseData } from './parseData';
+import { parseThesData, parseDictData } from './parseData';
 import { PrintThesaurus } from './PrintThesaurus';
+import { PrintDictionary } from './PrintDictionary';
 //import { findByLabelText } from '@testing-library/react';
 
 const createStyles = makeStyles(() => ({
@@ -20,7 +21,7 @@ const createStyles = makeStyles(() => ({
   },
 }));
 
-export function useGetThesaurus(searchWord, setAllDataArray) {
+export function useGetThesaurus(searchWord, setThesResults) {
   useEffect(() => {
     if (searchWord.length > 0) {
       fetch(
@@ -28,32 +29,53 @@ export function useGetThesaurus(searchWord, setAllDataArray) {
       )
         .then((response) => response.json())
         .then((data) => {
-          setAllDataArray(data);
+          setThesResults(data);
         });
     }
-  }, [searchWord, setAllDataArray]);
+  }, [searchWord, setThesResults]);
+  return;
+}
+
+export function useGetDictionary(searchWord, setDictResults) {
+  useEffect(() => {
+    if (searchWord.length > 0) {
+      fetch(
+        `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${searchWord}?key=${process.env.REACT_APP_KEY_DICTIONARY}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setDictResults(data);
+        });
+    }
+  }, [searchWord, setDictResults]);
   return;
 }
 
 export function SearchResults(props) {
   const myStyles = createStyles();
-  const [results, setResults] = useState([]);
+  const [thesResults, setThesResults] = useState([]);
+  const [dictResults, setDictResults] = useState([]);
 
-  useGetThesaurus(props.searchWord, setResults);
+  useGetThesaurus(props.searchWord, setThesResults);
+  useGetDictionary(props.searchWord, setDictResults);
 
-  const data = parseData(results);
-  const dataTemp =
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Section 1.10.32 of de Finibus Bonorum et Malorum, written by Cicero in 45 BC Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?';
+  const thesData = parseThesData(thesResults);
+  const dictData = parseDictData(dictResults, props.searchWord);
+
   //JSON.stringify(data);
-  console.log('data: ', data);
+  console.log('dataThes: ', thesData);
+  console.log('dictResults: ', dictResults);
 
   return (
     <div className={myStyles.container}>
       <div className={myStyles.subContainer}>
-        <PrintThesaurus data={data} />
+        <PrintThesaurus data={thesData} />
       </div>
 
-      <div className={myStyles.subContainer}>dictionary: {dataTemp}</div>
+      <div className={myStyles.subContainer}>
+        <PrintDictionary data={dictData}></PrintDictionary>
+        <pre>{JSON.stringify(dictData, null, 2)}</pre>
+      </div>
     </div>
   );
   // return <Typography variant="body1">This is the thesaurus div</Typography>;
