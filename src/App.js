10a403163126aad57683901900e84cd9d3602cfb
Isa Virtaoja <isa.virtaoja@gmail.com>
2020-09-10 10:10:07 +0300

    Clean file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React, { useState } from 'react';
 import { SearchResults } from './SearchResults.js';
-import { SearchWord } from './SearchWord.js';
+import { SearchButton } from './SearchButton.js';
+// import { createMuiTheme } from 'material-ui/core/styles';
+// import { MuiThemeProvider } from 'material-ui/styles';
+
+// const theme = createMuiTheme({
+//   typography: {
+//     subtitle1: {
+//       fontSize: 12,
+//     },
+//     body1: {
+//       fontWeight: 500,
+//     },
+//     button: {
+//       fontStyle: 'italic',
+//     },
+//   },
+// });
 
 function App() {
   const [searchWord, setSearchWord] = useState('');
 
-  function onSetSearchWord(textValue) {
-    setSearchWord(textValue);
-    // setSearchWord('design'); //remove this when not needed anymore
-  }
+  // function onSetSearchWord(textValue) {
+  //   setSearchWord(textValue);
+  //   // setSearchWord('design'); //remove this when not needed anymore
+  // }
 
   return (
     <div className="App">
       {/* <CreateHeader /> */}
-      <SearchWord onSetSearchWord={onSetSearchWord} />
+      <SearchButton setSearchWord={setSearchWord} />
       <SearchResults searchWord={searchWord} />
     </div>
   );
