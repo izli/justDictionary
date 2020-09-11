e71cf63d5bd785752d8bd72449f2c4394eaff585
Isa Virtaoja <isa.virtaoja@gmail.com>
2020-09-11 13:09:45 +0300

    Change functions into one recursive one. No other code cleaning (see next commit for those)

--- src/PrintDictionary.js
+++ src/PrintDictionary.js
@@ -100,7 +100,8 @@ export function PrintDictionary(props) {
       );
       return <div className={myStyles.allValuesAr}>{allValuesArray}</div>;
     } else {
-      let allRes = valuesHasArrays(props.data[typeVal].values, myStyles);
+      // let allRes = valuesHasArrays(props.data[typeVal].values, myStyles);
+      let allRes = recursiveHandling(props.data[typeVal].values, myStyles);
       let type = props.data[typeVal].type;
       type = type.charAt(0).toUpperCase() + type.slice(1);
       allValuesArray.push(
@@ -110,147 +111,180 @@ export function PrintDictionary(props) {
         </div>
       );
     }
-    debugger;
+    // debugger;
   }
-  debugger;
+  // debugger;
   return <div className={myStyles.allValuesAr}>{allValuesArray}</div>;
 }
 
-function valuesHasArrays(allValuesArrays, myStyles) {
-  // debugger;
-  let valResAr = [];
-  for (
-    let valuesLevel = 0;
-    valuesLevel < allValuesArrays.length;
-    valuesLevel++
-  ) {
-    if (Array.isArray(allValuesArrays[valuesLevel])) {
-      let subLevelArray = allValuesArrays[valuesLevel];
-      let subArRes = handleSubArrays(subLevelArray, myStyles);
-      valResAr.push(<div className={myStyles.containerVal}>{subArRes}</div>);
+function recursiveHandling(array, myStyles) {
+  let resultArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      let subArray = array[i];
+      let subArrayRes = recursiveHandling(subArray, myStyles);
+      resultArray.push(
+        <div className={myStyles.containerVal}>{subArrayRes}</div>
+      );
     } else {
       //No subArrays
-      let isFirst = handleSn(allValuesArrays, valuesLevel);
-      valResAr.push(
+      let isFirst = handleSn(array, i);
+      resultArray.push(
         <div className={myStyles.descExContainer}>
           <Typography
             className={isFirst ? myStyles.descLevel1 : myStyles.descLevel2}
           >
-            {allValuesArrays[valuesLevel].desc}
+            {array[i].desc}
           </Typography>
-          {allValuesArrays[valuesLevel].example.length > 0 && (
+          {array[i].example.length > 0 && (
             <Typography
               className={isFirst ? myStyles.exampleLv1 : myStyles.exampleLv2}
             >
-              {allValuesArrays[valuesLevel].example}
+              {array[i].example}
             </Typography>
           )}
         </div>
       );
     }
   }
-  // debugger;
-  return valResAr;
+  return resultArray;
 }
 
-function handleSubArrays(subLevelArray, myStyles) {
-  let subArRes = [];
-  for (let subLevel = 0; subLevel < subLevelArray.length; subLevel++) {
-    if (Array.isArray(subLevelArray[subLevel])) {
-      let subSubLevelArray = subLevelArray[subLevel];
-      let subSubRes = handleSubSubArrays(subSubLevelArray, myStyles);
-      subArRes.push(<div className={myStyles.subContainer}>{subSubRes}</div>);
-    } else {
-      //No subArrays
-      let isFirst = handleSn(subLevelArray, subLevel);
-      subArRes.push(
-        <div className={myStyles.descExContainer}>
-          <Typography
-            className={isFirst ? myStyles.descLevel1 : myStyles.descLevel2}
-          >
-            {subLevelArray[subLevel].desc}
-          </Typography>
-          {subLevelArray[subLevel].example.length > 0 && (
-            <Typography
-              className={isFirst ? myStyles.exampleLv1 : myStyles.exampleLv2}
-            >
-              {subLevelArray[subLevel].example}
-            </Typography>
-          )}
-        </div>
-      );
-    }
-  }
-  // debugger;
-  return subArRes;
-}
+// function valuesHasArrays(allValuesArrays, myStyles) {
+//   // debugger;
+//   let valResAr = [];
+//   for (
+//     let valuesLevel = 0;
+//     valuesLevel < allValuesArrays.length;
+//     valuesLevel++
+//   ) {
+//     if (Array.isArray(allValuesArrays[valuesLevel])) {
+//       let subLevelArray = allValuesArrays[valuesLevel];
+//       let subArRes = handleSubArrays(subLevelArray, myStyles);
+//       valResAr.push(<div className={myStyles.containerVal}>{subArRes}</div>);
+//     } else {
+//       //No subArrays
+//       let isFirst = handleSn(allValuesArrays, valuesLevel);
+//       valResAr.push(
+//         <div className={myStyles.descExContainer}>
+//           <Typography
+//             className={isFirst ? myStyles.descLevel1 : myStyles.descLevel2}
+//           >
+//             {allValuesArrays[valuesLevel].desc}
+//           </Typography>
+//           {allValuesArrays[valuesLevel].example.length > 0 && (
+//             <Typography
+//               className={isFirst ? myStyles.exampleLv1 : myStyles.exampleLv2}
+//             >
+//               {allValuesArrays[valuesLevel].example}
+//             </Typography>
+//           )}
+//         </div>
+//       );
+//     }
+//   }
+//   // debugger;
+//   return valResAr;
+// }
 
-function handleSubSubArrays(subSubLevelArray, myStyles) {
-  let subSubResAr = [];
-  for (
-    let subsubLevel = 0;
-    subsubLevel < subSubLevelArray.length;
-    subsubLevel++
-  ) {
-    if (Array.isArray(subSubLevelArray[subsubLevel])) {
-      let sub3LevelArray = subSubLevelArray[subsubLevel];
-      let sub3Res = handle3SubArrays(sub3LevelArray, myStyles);
-      subSubResAr.push(
-        <div className={myStyles.subSubContainer}>{sub3Res}</div>
-      );
-    } else {
-      let isFirst = handleSn(subSubLevelArray, subsubLevel);
-      subSubResAr.push(
-        <div className={myStyles.descExContainer}>
-          <Typography
-            className={isFirst ? myStyles.descLevel1 : myStyles.descLevel2}
-          >
-            {subSubLevelArray[subsubLevel].desc}
-          </Typography>
-          {subSubLevelArray[subsubLevel].example.length > 0 && (
-            <Typography
-              className={isFirst ? myStyles.exampleLv1 : myStyles.exampleLv2}
-            >
-              {subSubLevelArray[subsubLevel].example}
-            </Typography>
-          )}
-        </div>
-      );
-    }
-  }
-  // debugger;
-  return subSubResAr;
-}
+// function handleSubArrays(subLevelArray, myStyles) {
+//   let subArRes = [];
+//   for (let subLevel = 0; subLevel < subLevelArray.length; subLevel++) {
+//     if (Array.isArray(subLevelArray[subLevel])) {
+//       let subSubLevelArray = subLevelArray[subLevel];
+//       let subSubRes = handleSubSubArrays(subSubLevelArray, myStyles);
+//       subArRes.push(<div className={myStyles.subContainer}>{subSubRes}</div>);
+//     } else {
+//       //No subArrays
+//       let isFirst = handleSn(subLevelArray, subLevel);
+//       subArRes.push(
+//         <div className={myStyles.descExContainer}>
+//           <Typography
+//             className={isFirst ? myStyles.descLevel1 : myStyles.descLevel2}
+//           >
+//             {subLevelArray[subLevel].desc}
+//           </Typography>
+//           {subLevelArray[subLevel].example.length > 0 && (
+//             <Typography
+//               className={isFirst ? myStyles.exampleLv1 : myStyles.exampleLv2}
+//             >
+//               {subLevelArray[subLevel].example}
+//             </Typography>
+//           )}
+//         </div>
+//       );
+//     }
+//   }
+//   // debugger;
+//   return subArRes;
+// }
 
-function handle3SubArrays(sub3LevelArray, myStyles) {
-  let sub3ArResAr = [];
-  for (let sub3Level = 0; sub3Level < sub3LevelArray.length; sub3Level++) {
-    if (Array.isArray(sub3LevelArray[sub3Level])) {
-      console.log('TOO DEEP!');
-      debugger;
-      return <div>TOO DEEP</div>;
-    } else {
-      let isFirst = handleSn(sub3LevelArray, sub3Level);
-      sub3ArResAr.push(
-        <div className={myStyles.descExContainer}>
-          <Typography
-            className={isFirst ? myStyles.descLevel1 : myStyles.descLevel2}
-          >
-            {sub3LevelArray[sub3Level].desc}
-          </Typography>
-          {sub3LevelArray[sub3Level].example.length > 1 && (
-            <Typography
-              className={isFirst ? myStyles.exampleLv1 : myStyles.exampleLv2}
-            >
-              {sub3LevelArray[sub3Level].example}
-            </Typography>
-          )}
-        </div>
-      );
-    }
-  }
-  return sub3ArResAr;
-}
+// function handleSubSubArrays(subSubLevelArray, myStyles) {
+//   let subSubResAr = [];
+//   for (
+//     let subsubLevel = 0;
+//     subsubLevel < subSubLevelArray.length;
+//     subsubLevel++
+//   ) {
+//     if (Array.isArray(subSubLevelArray[subsubLevel])) {
+//       let sub3LevelArray = subSubLevelArray[subsubLevel];
+//       let sub3Res = handle3SubArrays(sub3LevelArray, myStyles);
+//       subSubResAr.push(
+//         <div className={myStyles.subSubContainer}>{sub3Res}</div>
+//       );
+//     } else {
+//       let isFirst = handleSn(subSubLevelArray, subsubLevel);
+//       subSubResAr.push(
+//         <div className={myStyles.descExContainer}>
+//           <Typography
+//             className={isFirst ? myStyles.descLevel1 : myStyles.descLevel2}
+//           >
+//             {subSubLevelArray[subsubLevel].desc}
+//           </Typography>
+//           {subSubLevelArray[subsubLevel].example.length > 0 && (
+//             <Typography
+//               className={isFirst ? myStyles.exampleLv1 : myStyles.exampleLv2}
+//             >
+//               {subSubLevelArray[subsubLevel].example}
+//             </Typography>
+//           )}
+//         </div>
+//       );
+//     }
+//   }
+//   // debugger;
+//   return subSubResAr;
+// }
+
+// function handle3SubArrays(sub3LevelArray, myStyles) {
+//   let sub3ArResAr = [];
+//   for (let sub3Level = 0; sub3Level < sub3LevelArray.length; sub3Level++) {
+//     if (Array.isArray(sub3LevelArray[sub3Level])) {
+//       console.log('TOO DEEP!');
+//       debugger;
+//       return <div>TOO DEEP</div>;
+//     } else {
+//       let isFirst = handleSn(sub3LevelArray, sub3Level);
+//       sub3ArResAr.push(
+//         <div className={myStyles.descExContainer}>
+//           <Typography
+//             className={isFirst ? myStyles.descLevel1 : myStyles.descLevel2}
+//           >
+//             {sub3LevelArray[sub3Level].desc}
+//           </Typography>
+//           {sub3LevelArray[sub3Level].example.length > 1 && (
+//             <Typography
+//               className={isFirst ? myStyles.exampleLv1 : myStyles.exampleLv2}
+//             >
+//               {sub3LevelArray[sub3Level].example}
+//             </Typography>
+//           )}
+//         </div>
+//       );
+//     }
+//   }
+//   return sub3ArResAr;
+// }
 
 function handleSn(array, position) {
   let acceptable = 'a|^\\d$';
