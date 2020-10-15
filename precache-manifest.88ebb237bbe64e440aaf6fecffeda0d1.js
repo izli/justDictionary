0aa7ce88061df376d0299b64e9b0c0f6ac6984aa
Isa Virtaoja <isa.virtaoja@gmail.com>
2020-10-15 20:46:48 +0300

    Updates

---
+++ precache-manifest.88ebb237bbe64e440aaf6fecffeda0d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bff7302729e4c25307eab8428210a88b",
+    "url": "/justDictionary/index.html"
+  },
+  {
+    "revision": "0f7bb424aae20049c62c",
+    "url": "/justDictionary/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7bc2c3ba1228a9e4b839",
+    "url": "/justDictionary/static/js/2.a68c87d6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/justDictionary/static/js/2.a68c87d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f7bb424aae20049c62c",
+    "url": "/justDictionary/static/js/main.77bfecdd.chunk.js"
+  },
+  {
+    "revision": "b3e110eddf0af5cd67ff",
+    "url": "/justDictionary/static/js/runtime-main.4d7f8a58.js"
+  }
+]);
